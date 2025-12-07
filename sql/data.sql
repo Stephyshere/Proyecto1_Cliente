87744a0809cf8f122f983101997280150ad29a88
Estefania <estefaniacastellanos802@gmail.com>
2025-12-07 18:45:31 +0100

    Logros actu

--- sql/data.sql
+++ sql/data.sql
@@ -50,8 +50,20 @@ INSERT IGNORE INTO logros (id, title, description, icon_url, clave_interna) VALU
 (1, 'Amor Eterno', 'Consigue que tu gato te ame al máximo.', 'LOGRO_GATO-TEAMA.png', 'love_max'),
 (2, 'Hasta la vista', 'Tu gato ha pasado a mejor vida.', 'LOGRO_GATO-RIP.png', 'rip'),
 (3, 'Enemigo Público', 'Tu gato te odia con toda su alma.', 'LOGRO_GATO-ODIO.png', 'hate_max'),
-(4, 'Gato Zen', 'Paz interior alcanzada.', 'LOGRO_GATOZEN.png', 'zen'),
-(5, 'Bollito', 'Has adoptado un gato Bollito.', 'LOGRO_BOLLITO.jpeg', 'gato_bollito'),
+(4, 'Gato Zen', 'Tu gato se duerme tras 5 min sin actividad.', 'LOGRO_GATOZEN.png', 'zen'),
+(5, 'Bollito', 'Has adoptado un gato Bollito.', 'LOGRO_BOLLITO.png', 'gato_bollito'),
 (6, 'Banana Cat', 'Has conseguido un gato Banana.', 'LOGRO_GATO-BANANA.png', 'gato_banana'),
 (7, 'Gato Perli', 'Has adoptado a la legendaria Perli.', 'LOGRO_PERLI.jpeg', 'gato_perli'),
-(8, 'Gato Chino', 'Has adoptado un Gato Chino de la suerte.', 'logroGatoChino.jpg', 'gato_chino');
+(8, 'Gato Chino', 'Has adoptado un Gato Chino de la suerte.', 'LOGRO-GATO-SUERTE.jpg', 'gato_chino');
+(9, 'Abandonado', 'Tu gato ha decidido abandonarte.', 'LOGRO_GATO-ABANDONO.png', 'abandonado');
+(10, 'Solid Snake', 'Le has dado una caja a tu gato y se siente como Solid Snake.', 'LOGRO_GATO-SNAKE.png', 'solid_snake');
+(11, 'Hipocondriaco', 'Le has dado medicina a tu gato con la salud al máximo "por si acaso".', 'LOGRO_GATO-HIPO.png', 'hipocondriaco');
+(12, 'Indigestion', ' Le has dado de comer justo después de jugar o asustarlo.', 'LOGRO_GATO-INDIGESTION.png', 'indigestion');
+(13, 'Mision Suicida', '¿De verdad ibas a bañar a tu gato con lo que te odia?', 'LOGRO_GATO-MISION-SUICIDA.png', 'mision_suicida');
+(14, 'Corazon de Piedra', 'Tu gato tiene la salud muy baja y lo has ignorado.', 'LOGRO_GATO-CORAZON-PIEDRA.png', 'corazon_de_piedra');
+(15, 'Gato Zombie', '¡Has curado a tu gato justo antes de que muriera!', 'LOGRO_GATOZOMBIE.png', 'gato_zombie');
+(16, 'Dedos Sangrantes', 'Has acariciado a tu gato cuando te odiaba mucho.', 'LOGRO_GATO-DEDOS-SANGRANTES.png', 'dedos_sangrantes');
+(17, 'Amor tragico', 'Tu gato a muerto amandote por completo', 'LOGRO_GATO-AMIGO-FIEL.png', 'amigo_fiel');
+(18, 'Rencor Eterno', 'Tu gato ha muerto odiandote con toda su alma.', 'LOGRO_GATO-RENCOROSO.png', 'rencor_eterno');
+(19, 'Gato Amoroso', 'El gato que has adoptado es extremadamente cariñoso aunque no te conozca.', 'LOGRO_GATO-AMOROSO.png', 'gato_amoroso');
+(20, 'Gato Odioso', 'El gato que has adoptado te odia desde el primer momento.', 'LOGRO_GATO-ODIOSO.png', 'gato_odioso');
